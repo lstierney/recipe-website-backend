190e2334b083a4e2d6e534660aa49545587a8b36
Lawrence Tierney <lawrence.tierney@protonmail.com>
2023-03-14 15:16:53 +0000

    MVR-80 Added REST endpoint for searching by tagname

--- src/main/java/com/lstierneyltd/recipebackend/repository/RecipeRepository.java
+++ src/main/java/com/lstierneyltd/recipebackend/repository/RecipeRepository.java
@@ -2,11 +2,15 @@ package com.lstierneyltd.recipebackend.repository;
 
 import com.lstierneyltd.recipebackend.entities.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
     List<RecipeIdAndName> findAllRecipeIdAndNameBy();
+
+    @Query("SELECT r FROM Recipe r JOIN r.tags t WHERE t.name = ?1")
+    List<Recipe> findByTagName(String name);
 
     interface RecipeIdAndName {
         int getId();
