44097bfa3c3dca83d7638e79007ddb6c321e3a21
Lawrence Tierney <lawrence.tierney@protonmail.com>
2024-05-23 09:55:59 +0100

    MVR-303 Add cache control to /images
    Add WebConfig class for resource handling and cache control
    
    A new WebConfig class has been added to manage resource handling, specifically for '/images' using Spring's WebMvcConfigurer interface. The configuration implements cache control for a maximum age of 365 days. Additionally, the 'spring.web.resources.static-locations' line in the application.properties file has been commented out.

---
+++ src/main/java/com/lstierneyltd/recipebackend/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.lstierneyltd.recipebackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:/opt/recipe-website/images/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS).cachePublic());
+    }
+}
